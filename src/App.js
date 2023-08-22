428b85dda0539530d3f5babfa2a8affd8a7271a7
JxstWieslaw <72025253+JxstWieslaw@users.noreply.github.com>
2023-08-22 18:12:28 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
                 Title
               </div>
             </div>
-            <div className="options-bar h-1/6 mb-2 p-1">
+            <div className="options-bar h-1/6 pb-1">
               <div onMouseEnter={disableScroll} onMouseLeave={enableScroll}>
                 <ScrollMenu
                   LeftArrow={LeftArrow}
