a31aeff2cb35cc3ed567e97673a0626ebfba79fb
JxstWieslaw <72025253+JxstWieslaw@users.noreply.github.com>
2023-08-22 17:49:22 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -114,7 +114,9 @@ function App() {
         <div className="menu">
           <div className="lg:absolute lg:bottom-1/4 flex flex-col  h-full lg:h-4/6 w-full lg:w-1/2">
             <div className="title-bar h-1/6 rounded-t-3xl">
-              <div className="font-sans h-full text-3xl text-white ">Title</div>
+              <div className="font-sans p-1 mb-2 h-full text-3xl text-white ">
+                Title
+              </div>
             </div>
             <div className="options-bar h-1/6 p-1">
               <div onMouseEnter={disableScroll} onMouseLeave={enableScroll}>
